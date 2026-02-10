ed93d776f30a99095dfcc9de0103eafafc0cf542
s3loy <1915276056@qq.com>
2026-02-10 20:36:43 +0800

    feat: refactor route

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,14 +7,11 @@ import (
 	"strconv"
 
 	"snippetbox.s3loy.tech/internal/models"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -28,7 +25,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -50,21 +49,19 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
 
-	title := "0 snail"
-	content := "0 snail\nClimb Mount Fuji,\nBut slowly,slowly!\n\n- Kobayashi Issa"
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
 	expires := 7
-
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf(("/snippet/view?id=%d"), id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module snippetbox.s3loy.tech
 
 go 1.25.7
 
-require github.com/go-sql-driver/mysql v1.9.3
-
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/justinas/alice v1.2.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/justinas/alice v1.2.0
 )
+
+require filippo.io/edwards25519 v1.1.0 // indirect
