7e4e2493893d2ca5127ca92b2103e95b32868c28
s3loy <1915276056@qq.com>
2026-03-04 13:45:25 +0800

    feat: add validator for userSignup

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -4,4 +4,8 @@ import (
 	"errors"
 )
 
-var ErrNoRecord = errors.New("models: no matching recort found")
+var (
+	ErrNoRecord           = errors.New("models: no matching recort found")
+	ErrInvalidCredentials = errors.New("models: indalid credentials")
+	ErrDuplicateEmail     = errors.New("models: duplicate email")
+)
