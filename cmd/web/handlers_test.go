23d619fb7c1fee9609699087805013e2b4a503b2
s3loy <1915276056@qq.com>
2026-03-09 01:44:14 +0800

    feat: refactor ping handler tests and add test utilities

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,32 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"snippetbox.s3loy.tech/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := newTestApplication(t)
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	ping(rr, r)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	rs := rr.Result()
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
+	code, _, body := ts.get(t, "/ping")
 
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	body = bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
